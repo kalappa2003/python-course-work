d8b8cb8ea636379faf6fa402271ce936fb3bec95
unknown <kalipa829@gmail.com>
2025-07-02 14:22:58 +0530

    ttt

---
+++ class work/cw4 python operators.py
@@ -0,0 +1,11 @@
+#1.Arithmetic operators
+a=20
+b=10
+print("Addition(+):",a+b) #Addition(+): 30
+print("Subtraction(-):",a-b) #Subtraction(-): 10
+print("Multiplication():",a*b) #Multiplication(): 200
+print("Division(/):",a/b) #Division(/): 2.0
+print("Floor Division(//):",a//b) #Floor Division(//): 2
+print("Modulus(%):",a%b) #Modulus(%): 0
+print("Modulus(%):",b%a) #Modulus(%): 10
+print("Exponentiation(*):",a**b) #Exponentiation(*): 10240000000000
