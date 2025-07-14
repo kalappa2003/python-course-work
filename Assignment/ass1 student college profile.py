300cc812f477a3d6f59a88f2233245aa2b8eb9f6
unknown <kalipa829@gmail.com>
2025-07-14 21:58:05 +0530

    ttt

---
+++ Assignment/ass1 student college profile.py
@@ -0,0 +1,14 @@
+student_info={"name":"babu","ID":"Ul6KN21S0080",
+              "Course":"Bsc","Combination":["computer science","mathematics"],
+         "library ID":"9431","Class room":["G008","S112"],
+              "CGPA":"74%"}
+print("student_information")
+print(f"student name{student_info['name']}")
+print(f"student ID{student_info['ID']}")
+print(f"student Course{student_info['Course']}")
+print(f"student Combination{student_info['Combination']}")
+print(f"student library ID{student_info['library ID']}")
+print(f"student Class room{student_info['Class room']}")
+print(f"student Class room{student_info['Class room']}")
+print(f"student CGPA{student_info['CGPA']}")
+
