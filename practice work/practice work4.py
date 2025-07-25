0648f5d1446ffb6b1bdea65df1d057aaeab45881
unknown <kalipa829@gmail.com>
2025-07-25 15:12:46 +0530

    ttt

---
+++ practice work/practice work4.py
@@ -0,0 +1,13 @@
+
+print("Right Angled Triangle")
+n=5
+for i in range(1,n+1):
+    print(" "*(n-i)+"*"*i)
+
+print("\n Inverted Right-Angled Triangle")
+for i in range(n,0,-1):
+    print("*"*i)
+
+print("\nPattern printing Series")
+for i in range(5):
+    print("*"*5)
