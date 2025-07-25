396c171a5ca94032e526ce4fe398c108462d9eb8
unknown <kalipa829@gmail.com>
2025-07-25 14:43:34 +0530

    ttt

---
+++ class work/cw13 if elif else statement.py
@@ -0,0 +1,22 @@
+
+#3. if-elif-else Statement
+
+#Weekend Budget Plan
+
+amount=int(input("Enter the Weekend Budget: "))
+if amount>20000:
+    print("Trip to Manali")
+elif amount>15000:
+    print("Go for Goa")
+elif amount>5000:
+    print("Araku")
+elif amount>2000:
+    print("Dinner Date")
+elif amount>1000:
+    print("Movie Date")
+elif amount>500:
+    print("Long Drive")
+elif amount>100:
+    print("Save the money for next weekend brooo!")
+else:
+    print("You can't even buy Alekhya Chitti pickles... Earn more brooo!")
