f6199a64d6dc2f5552c0784eeb17bbd0930483f8
unknown <kalipa829@gmail.com>
2025-07-25 14:35:49 +0530

    ttt

---
+++ class work/cw11 if statement.py
@@ -0,0 +1,17 @@
+
+#12. Conditional Statements
+
+#1.if Statement
+
+#Traffic Signals
+
+s=input("Enter the status(R O G) : ")
+
+if s=='R':
+    print("Stop")
+
+if s=='O':
+    print("Get Ready!")
+
+if s=='G':
+    print("Goooo0")
