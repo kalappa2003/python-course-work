4df75a51a2359528b5359f5540f26bb8b528bb3d
unknown <kalipa829@gmail.com>
2025-07-25 15:09:49 +0530

    ttt

---
+++ practice work/practice work2.py
@@ -0,0 +1,10 @@
+
+#Cheking Number (+ve, 0, -ve) using if-elif-else
+
+num=float(input("Enter a number: "))
+if num>0:
+    print("The entered number is +ve...")
+elif num<0:
+    print("The entered number is -ve...")
+else:
+    print("The entered number is 0...")
